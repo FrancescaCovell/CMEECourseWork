11c2a486b9b99a8b12bda16ae4c277c5d9174976
Francesca Covell <fc420@imperial.ac.uk>
2022-01-20 06:22:35 -0500

    updated README added comments to appy2.R and run_get_TreeHeight.sh

--- week3/code/run_get_TreeHeight.sh
+++ week3/code/run_get_TreeHeight.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#Author: Group3
+#Script: run_get_TreeHeight.sh
+#Des: Runs get_TreeHeight.r and get_TreeHeight.py on trees.csv from bash
+#Usage: bash run_get_TreeHeight.sh  (in terminal)
+#Dep: 
+
 # Run get_TreeHieght R script on trees.csv
 Rscript get_TreeHeight.R ../data/trees.csv 
 
